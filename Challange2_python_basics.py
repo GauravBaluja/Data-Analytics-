3b42b42ca23e85b0f236fe7352b918261fd7005f
Gaurav_Baluja <gauravbaluja22@gmail.com>
2024-01-08 01:03:41 +0530

    Update Challange2_python_basics.py

--- Challange2_python_basics.py
+++ Challange2_python_basics.py
@@ -1,4 +1,4 @@
-#Programming Challenge: Miles Per Gallon
+#Programming Challenge2: Miles Per Gallon
 #For this exercise, you will create a program that approximates the number of miles per gallon that a car gets.
 
 from random import randint
@@ -7,4 +7,18 @@ gallan_fuel_tank = randint(10,25)  # gallan_fuel_tank = the number of gallons of
 milage = randint(200,500)   # milage = the miles that the car can travel on a full tank before needing a refuel.
 MPG = milage/gallan_fuel_tank
 print(round(MPG,2))
+
+
+#Challange3
+#grading system for school teacher
+grade = int(input("Enter student Score: "))
+if grade >= 90:
+    print("Grade A")
+elif grade >= 80:
+        print("Grade B")
+elif grade >= 70:
+        print("Grade C")
+elif grade >= 60:
+        print("Grade D")
+else: print("grade F")
 
